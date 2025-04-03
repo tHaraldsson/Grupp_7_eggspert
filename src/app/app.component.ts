84d305beb1bb17c62cef80b03b1f205971169b80
EricBO2 <eric.carlsson@stud.sti.se>
2025-04-03 09:33:40 +0200

    fixed the routerLink and added new backround image for desktop timer Eric.C

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 import { HamburgerMenuComponent } from "./hamburger-menu/hamburger-menu.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HamburgerMenuComponent],
+  imports: [RouterOutlet, HamburgerMenuComponent,RouterModule],
 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css',]
